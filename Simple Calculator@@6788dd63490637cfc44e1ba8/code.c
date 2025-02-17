1fd1da010afb7e1ca528743251ab1a3cc3793937
Bhavika <bhavika12073b@gmail.com>
2025-02-18 00:47:01 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -13,9 +13,9 @@ int main() {
     }else if(c=='*'){
         printf("%d", a*b);
     }else if(c=='/'){
-        printf("%d", a/b);
-    }else{
-        printf("error");
-    }
+        printf("%d", a/b);}
+    // }else{
+    //     printf("error");
+    // }
     return 0;
 }
